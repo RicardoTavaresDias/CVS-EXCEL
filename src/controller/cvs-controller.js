d0aa1159f8b7b40ac669e2b8bd774fbd47b0823d
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-04-17 14:50:43 -0300

    versão 1.0.1 - Comentarios

--- src/controller/cvs-controller.js
+++ src/controller/cvs-controller.js
@@ -4,11 +4,18 @@ export class CvsController {
   createCVS(request, response){
     try {
       const data = request.body 
+
+      // Cria uma planilha a partir dos dados
       const sheet = XLSX.utils.json_to_sheet(data)
+
+      // Cria uma nova pasta de trabalho (book)
       const book = XLSX.utils.book_new()
       XLSX.utils.book_append_sheet(book, sheet, 'request')
+
+      // Salva o arquivo
       XLSX.writeFile(book, 'teste.xlsx')
       response.status(200).json({ message: 'Salvo com sucesso, no arquivo CVS!' })
+
     }catch(error) {
       response.status(422).json({ message: 'Error ao criar planilha CVS!', error: error })
     }
@@ -16,12 +23,17 @@ export class CvsController {
 
   indexCVS(request, response){
     try {
+      // Caminho do arquivo Excel e Lê o nome das planilhas
       const file = XLSX.readFile("./teste.xlsx")
       const sheetName = file.SheetNames[0]
-      const sheet = file.Sheets[sheetName]
-      const data = XLSX.utils.sheet_to_json(sheet, {  header: 2 })
 
+      // Pega a primeira planilha
+      const sheet = file.Sheets[sheetName]
+
+      // Converte a planilha para JSON
+      const data = XLSX.utils.sheet_to_json(sheet, {  header: 2 })
       response.status(200).json(data)
+
     }catch(error) {
       response.status(422).json({ message: 'Error ao abrir a planilha CVS!', error: error })
     }
@@ -29,12 +41,17 @@ export class CvsController {
 
   update(request, response){
     try {
+      // Caminho do arquivo Excel e Lê o nome das planilhas
       const file = XLSX.readFile("./teste.xlsx")
       const sheetName = file.SheetNames[0]
+
+      // Pega a primeira planilha
       const sheet = file.Sheets[sheetName]
 
+      // Converte a planilha para JSON
       const data = XLSX.utils.sheet_to_json(sheet, {  header: 2 })
 
+      // Atualizando dado
       const update = data.map(value => {
         if (value.id === request.body.id){
             request.body.idade ? value.idade = Number(request.body.idade) : value.idade
@@ -43,12 +60,17 @@ export class CvsController {
         return value
       })
 
+      // Cria uma planilha a partir dos dados
       const sheetNew = XLSX.utils.json_to_sheet(update)
+
+      // Cria uma nova pasta de trabalho (book), criando nova planilha com dados novos
       const book = XLSX.utils.book_new()
       XLSX.utils.book_append_sheet(book, sheetNew, 'request')
-      XLSX.writeFile(book, './teste.xlsx')
 
+      // Salva o arquivo
+      XLSX.writeFile(book, './teste.xlsx')
       response.status(200).json({ message: 'Arquivo alterado com sucesso, no arquivo CVS!' })
+
     }catch(error) {
       response.status(422).json({ message: 'Error a atualizar dados na planilha CVS!', error: error })
     }
@@ -56,25 +78,36 @@ export class CvsController {
 
   delete(request, response){
     try {
+      // Caminho do arquivo Excel e Lê o nome das planilhas
       const file = XLSX.readFile("./teste.xlsx")
       const sheetName = file.SheetNames[0]
+
+      // Pega a primeira planilha
       const sheet = file.Sheets[sheetName]
 
+      // Converte a planilha para JSON
       const data = XLSX.utils.sheet_to_json(sheet, {  header: 2 })
 
+      // Verifica se o usuario existe na planilha
       const filterUser = data.filter(value => value.id === Number(request.params.id))
       if(!filterUser.length){
         return response.status(400).json({ message: 'Usuario não cadastrado!' })
       } 
 
+      // remove o usuario
       const remove = data.filter(value => value.id !== Number(request.params.id))
     
+      // Cria uma planilha a partir dos dados
       const sheetNew = XLSX.utils.json_to_sheet(remove)
+
+      // Cria uma nova pasta de trabalho (book), criando nova planilha com dados novos
       const book = XLSX.utils.book_new()
       XLSX.utils.book_append_sheet(book, sheetNew, 'request')
-      XLSX.writeFile(book, './teste.xlsx')
 
+      // Salva o arquivo
+      XLSX.writeFile(book, './teste.xlsx')
       response.status(200).json({ message: 'Usuario removido com sucesso, no arquivo CVS!' })
+
     }catch(error) {
       response.status(422).json({ message: 'Error ao remover usuario na planilha CVS!', error: error })
     }
@@ -82,25 +115,36 @@ export class CvsController {
 
   insert(request, response){
     try {
+      // Caminho do arquivo Excel e Lê o nome das planilhas
       const file = XLSX.readFile("./teste.xlsx")
       const sheetName = file.SheetNames[0]
+
+      // Pega a primeira planilha
       const sheet = file.Sheets[sheetName]
 
+      // Converte a planilha para JSON
       const insert = XLSX.utils.sheet_to_json(sheet, {  header: 2 })
       
+      // Verifica se o usuario já existe na planilha
       const filterUser = insert.filter(value => value.name === request.body.name)
       if(filterUser.length){
         return response.status(400).json({ message: 'Usuario já cadastrado!' })
       }
 
+      // Add novo usuario na planilha
       insert.push(request.body)
 
+      // Cria uma planilha a partir dos dados
       const sheetNew = XLSX.utils.json_to_sheet(insert)
+
+      // Cria uma nova pasta de trabalho (book), criando nova planilha com dados novos
       const book = XLSX.utils.book_new()
       XLSX.utils.book_append_sheet(book, sheetNew, 'request')
-      XLSX.writeFile(book, './teste.xlsx')
 
+      // Salva o arquivo
+      XLSX.writeFile(book, './teste.xlsx')
       response.status(200).json({ message: 'Usuario Cadastrado com sucesso, no arquivo CVS!' })
+
     }catch(error) {
       response.status(422).json({ message: 'Error ao inserir usuario na planilha CVS!', error: error })
     }

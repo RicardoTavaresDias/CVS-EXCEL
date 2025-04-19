0afc81a3d3284bf458d7a036b7d9239794bf2993
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-04-19 20:11:28 -0300

    versão 2.2.8

--- src/controller/exceljs-controller.js
+++ src/controller/exceljs-controller.js
@@ -4,21 +4,21 @@ import XLSX from 'xlsx'
 export class ExceljsController {
   async insert(request, response){
     try {
-      // Acessando o arquivo e acessando a aba Ativos
+      // Acessando o arquivo e acessando a aba Ativos.
       const workbook = new ExcelJS.Workbook()
       await workbook.xlsx.readFile("./teste.xlsx")
       const sheet = workbook.getWorksheet("Ativos")
 
-      // Levantando todas os dados cadastrados na planilha com biblioteca XLSX
+      // Levantando todas os dados cadastrados na planilha com biblioteca XLSX.
       const xlsxFile = XLSX.readFile("./teste.xlsx")
       const xlsxSheetName = xlsxFile.SheetNames[0]
       const xlsxSheet = xlsxFile.Sheets[xlsxSheetName]
       const data = XLSX.utils.sheet_to_json(xlsxSheet, { header: 2 })
 
-      // Linha
+      // Linha.
       let rowIndex = data.length + 1
 
-      // Loop para inserir os dados na planilha
+      // Loop para inserir os dados na planilha.
       for(const item of request.body){
         const row = sheet.getRow(rowIndex)
 
@@ -31,7 +31,7 @@ export class ExceljsController {
         rowIndex++
       }
 
-      // Salva arquivo
+      // Salva arquivo.
       await workbook.xlsx.writeFile("teste.xlsx")
       response.status(200).json({ message: "Dados inseridos com sucesso!"})
 
